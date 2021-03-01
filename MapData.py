f1be07533d46577ba8d359ac8c5dec041b44a2e6
festevil <62298846+festevil@users.noreply.github.com>
2021-03-01 21:15:56 +0800

    Add files via upload

--- MapData.py
+++ MapData.py
@@ -5,16 +5,21 @@ import pandas
 import gmplot
 
 class MapData:
+    """Gives us the Map Data of the address specified from street and town"""
     def __init__(self,street, town):
         self.street = street
         self.town = town
     def __GeocodeCoor(self):
+        """Geocodes address to latitude and longitiude. Used in plotpoint."""
         GoogleAPI ='AIzaSyAT5ls2tvRCs9x0p4aVd7984HNocfpAOXE'
         geolocator = GoogleV3(api_key=GoogleAPI)
         loc = geolocator.geocode(self.street + " " + self.town)
         return loc
     
     def plotpoint(self):
+        """Returns a google map with the specified location from street and town."""
         location = self.__GeocodeCoor()
-        gmplot.GoogleMapPlotter(location.latitude, location.longitude, 20)
+        gmap = gmplot.GoogleMapPlotter(location.latitude, location.longitude, 20)
+        gmap.marker(location.latitude,location.longitude)
+
     
