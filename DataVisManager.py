85c6ef657791402cd14079a3630135bced47d8e6
LZXgithubZX <73070369+LZXgithubZX@users.noreply.github.com>
2021-03-09 22:58:59 +0800

    true to True

--- DataVisManager.py
+++ DataVisManager.py
@@ -3,7 +3,7 @@ import DataVisualisation as dv
 data = dv.readData()
 dv.barPriceVsTown(data)
 
-while (true):
+while (True):
     try:
         choice = int(input("Select 1 for barPriceVsTown, 2 for barPriceVsFlatType, 3 for pointPriceVsYear, 4 to exit: "))
     except ValueError:
