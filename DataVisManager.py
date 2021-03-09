f5270ac9c276acd6573b496c7b7ab296dfbd7e82
LZXgithubZX <73070369+LZXgithubZX@users.noreply.github.com>
2021-03-09 22:46:36 +0800

    Removed poorly written while condition
    while condition was always met so it didn't enter try-except

--- DataVisManager.py
+++ DataVisManager.py
@@ -1,20 +1,19 @@
 import DataVisualisation as dv
 
 data = dv.readData()
-dv.barPriceVsTown(data)
 choice = 1
 
-while (1 <= choice <= 3):
-    while type(choice) is not int:
-        try:
-            choice = int(input("Select 1 for barPriceVsTown, 2 for barPriceVsFlatType, 3 for pointPriceVsYear, 4 to exit: "))
-        except ValueError:
-            choice = int(input("This was not a valid input please try again: "))
+while (choice <= 3):
+    try:
+        choice = int(input("Select 1 for barPriceVsTown, 2 for barPriceVsFlatType, 3 for pointPriceVsYear, 4 to exit: "))
+    except ValueError:
+        print ("That was not a valid input, please try again.")
+        continue
     if choice == 1:
         dv.barPriceVsTown(data)
     elif choice == 2:
         dv.barPriceVsFlatType(data)
     elif choice == 3:
         dv.pointPriceVsYear(data)
-    else:
+    elif choice == 4:
         break
